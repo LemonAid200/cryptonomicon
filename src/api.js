dde4023e0de55de8967113be94fc4a2680e74cf2
Vsevolod <vsevolodehe@mail.ru>
2022-10-13 14:28:13 +0500

    integrated WS in api.js, extended WS class

--- src/api.js
+++ src/api.js
@@ -2,24 +2,66 @@
 const API_Key = 'f803a0614d11ffe8421ae96983ad4b1efe8ba29264d09309df3a6d9334f6169c'
 const allValuesLink = 'https://min-api.cryptocompare.com/data/all/coinlist?summary=true'
 
-export const loadTickersValues = () => {
-  if (tickersHandlers.size === 0) return
-  fetch(
-    `https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${[...tickersHandlers.keys()]
-    .join(',')}&api_key=${API_Key}`
-  )
-    .then(t => t.json())
-    .then(rawData => {
-      const updatedPrices = Object.fromEntries(
-        Object.entries(rawData).map(([key, value]) => [key, value])
-      )
-      Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
-        const handlers = tickersHandlers.get(currency) ?? []
+// export const loadTickersValues = () => {
+//   if (tickersHandlers.size === 0) return
+//   fetch(
+//     `https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${[...tickersHandlers.keys()]
+//     .join(',')}&api_key=${API_Key}`
+//   )
+//     .then(t => t.json())
+//     .then(rawData => {
+//       const updatedPrices = Object.fromEntries(
+//         Object.entries(rawData).map(([key, value]) => [key, value])
+//       )
+//       Object.entries(updatedPrices).forEach(([currency, newPrice]) => {
+//         const handlers = tickersHandlers.get(currency) ?? []
 
-        handlers.forEach(fn => fn(1 / newPrice, currency))
-      })
+//         handlers.forEach(fn => fn(1 / newPrice, currency))
+//       })
+//     })
+// }
+// setInterval(loadTickersValues, 6000)
+
+class CryptoWebSocket extends WebSocket {
+  subscribe (tickerName) {
+    if (this.readyState === CryptoWebSocket.OPEN) {
+      this.send(JSON.stringify(
+        {
+          action: 'SubAdd',
+          subs: [`5~CCCAGG~${tickerName}~USD`]
+        }
+      ))
+      return
+    }
+    this.addEventListener('open', () => {
+      this.send(JSON.stringify(
+        {
+          action: 'SubAdd',
+          subs: [`5~CCCAGG~${tickerName}~USD`]
+        }
+      ))
     })
+  }
+
+  unsubscribe (tickerName) {
+    this.send(JSON.stringify(
+      {
+        action: 'SubRemove',
+        subs: [`5~CCCAGG~${tickerName}~USD`]
+      }
+
+    ))
+  }
 }
+
+const socket = new CryptoWebSocket(`wss://streamer.cryptocompare.com/v2?api_key=${API_Key}`)
+socket.addEventListener('message', e => {
+  const message = JSON.parse(e.data)
+  if (message.TYPE === '5' && message.PRICE) {
+    const handlers = tickersHandlers.get(message.FROMSYMBOL) ?? []
+    handlers.forEach(fn => fn(message.PRICE, message.FROMSYMBOL))
+  }
+})
 
 export const getAllTickerNames = () => fetch(allValuesLink).then(coinList => coinList.json())
   .then(data => {
@@ -32,16 +74,15 @@ export const getAllTickerNames = () => fetch(allValuesLink).then(coinList => coi
   })
 
 const tickersHandlers = new Map()
-
 window.tickers = tickersHandlers
 
 export const subscribeToTicker = (ticker, cb) => {
   const subscribers = tickersHandlers.get(ticker) || []
   tickersHandlers.set(ticker, [...subscribers, cb])
+  socket.subscribe(ticker)
 }
 
 export const unsubscribeTicker = ticker => {
   tickersHandlers.delete(ticker)
+  socket.unsubscribe(ticker)
 }
-
-setInterval(loadTickersValues, 6000)
